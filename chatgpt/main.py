a5fd6e70d4c12d701e200bb490179dd6892fb785
Noah Sohn <gonoah510@gmail.com>
2023-04-08 15:26:09 -0700

    Update main.py

--- chatgpt/main.py
+++ chatgpt/main.py
@@ -5,10 +5,10 @@ openai.api_key = "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"
 
 # Set up the model and prompt
 model_engine = "text-davinci-003"
-prompt = "What do you think of hackathons?"
+prompt = "A hackathon (also known as a hack day, hackfest, datathon or codefest; a portmanteau of hacking and marathon) is an event where people engage in rapid and collaborative engineering over a relatively short period of time such as 24 or 48 hours. They are often run using agile software development practices, such as sprint-like design wherein computer programmers and others involved in software development, including graphic designers, interface designers, product managers, project managers, domain experts, and others collaborate intensively on engineering projects, such as software engineering. Can you please summarize this ?"
 
 # Generate a response
-completion = openai.Completion.create(
+Completion = openai.Completion.create(
     engine=model_engine,
     prompt=prompt,
     max_tokens=1024,
@@ -17,5 +17,5 @@ completion = openai.Completion.create(
     temperature=0.5,
 )
 
-response = completion.choices[0].text
+response = Completion.choices[0].text
 print(response)
