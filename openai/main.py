3dc8be85947fe64f3304d8337e33f7efc5efc7c1
Noah Sohn <gonoah510@gmail.com>
2023-04-08 15:45:59 -0700

    Update main.py

--- openai/main.py
+++ openai/main.py
@@ -3,9 +3,12 @@ import openai
 # Set up the OpenAI API client
 openai.api_key = "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"
 
+#Ask for user query
+query = input("Enter your query: ")
+
 # Set up the model and prompt
 model_engine = "text-davinci-003"
-prompt = "Placeholder ChaTGPT request"
+prompt = "Turn this into a query intended for a search engine: {}".format(query)
 
 # Generate a response
 Completion = openai.Completion.create(
